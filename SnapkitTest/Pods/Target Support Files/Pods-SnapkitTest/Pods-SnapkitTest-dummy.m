05876041457f4b5ed6ac560bdc1dcdf82f336d33
swapnil-salunke <swapnil.salunke.mobile@gmail.com>
2021-07-16 12:29:23 +0530

    Integrating Snapkit Pod

---
+++ SnapkitTest/Pods/Target Support Files/Pods-SnapkitTest/Pods-SnapkitTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SnapkitTest : NSObject
+@end
+@implementation PodsDummy_Pods_SnapkitTest
+@end
---
+++ SnapkitTest/SnapkitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapkitTest
+//
+//  Created by Salunke, Swapnil Uday on 16/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
